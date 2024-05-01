0ad380f6d0758e08582660edf2d78cfd5f20e695
mochki <grapefruitnights@protonmail.com>
2024-04-30 18:30:02 -0600

    appstate entites as array and rename

--- src/store/app-state.ts
+++ src/store/app-state.ts
@@ -7,14 +7,21 @@ export const useAppState = create(
   persist(
     (set, get) => ({
       view: "Research",
-      workspaceEntities: null,
+      workspaceEntities: [],
       updateView: (view) => set(() => ({ view })),
       updateWorkspaceEntities: (entity) =>
         set(() => ({
           // @ts-expect-error duh
-          workspaceEntities: { ...get().workspaceEntities, entity },
+          workspaceEntities: [...get().workspaceEntities, entity],
         })),
+      clearWorkspaceEntities: () => set(() => ({ workspaceEntities: [] })),
     }),
     { name: "app-state" }
   )
 );
+
+/** Entities
+ * type: task || pokemon
+ * data: { pkid, task, category, goal } || {}
+ * -> just use their shape
+ */
