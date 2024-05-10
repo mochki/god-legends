ebe0b56f8db92e060fadad4d3964e57b7d82c12a
mochki <grapefruitnights@protonmail.com>
2024-05-09 20:58:50 -0600

    almost got a lot worse. gudenuf

--- src/views/BoxLayout.tsx
+++ src/views/BoxLayout.tsx
@@ -4,6 +4,7 @@ import {Pokemon} from '../db';
 import {Sprites} from '../assets/sprites';
 import alphaUrl from '../assets/alpha.png';
 import {IoFemaleSharp, IoMaleSharp} from 'react-icons/io5';
+import {HiOutlineSparkles} from 'react-icons/hi2';
 
 const uniques = Object.values(Pokemon)
   // @ts-expect-error duh
@@ -43,8 +44,12 @@ export default function BoxLayout() {
                 onClick={handlePokemonClick(_.uid)}
               >
                 {_.shiny ?
-                  <img src={Sprites[_.pokemon]} className="hue-rotate-[140deg] sepia-[20%]" />
-                : <img src={Sprites[_.pokemon]} />}
+                  <img
+                    src={Sprites[_.pokemon]}
+                    title={_.form}
+                    className="hue-rotate-[140deg] sepia-[20%] contrast-150"
+                  />
+                : <img src={Sprites[_.pokemon]} title={_.form} />}
                 {/* the forms? lol */}
                 {_.alpha && (
                   <img
@@ -52,12 +57,16 @@ export default function BoxLayout() {
                     className="absolute h-2/6 w-2/6 bottom-1 right-1 saturate-50"
                   />
                 )}
+                {_.shiny && (
+                  <HiOutlineSparkles className="absolute h-1/4 w-1/4 top-1 right-1 text-amber-200" />
+                )}
                 {_.gender === 'Female' && (
                   <IoFemaleSharp className="absolute h-1/4 w-1/4 bottom-1 left-1 text-pink-300" />
                 )}
                 {_.gender === 'Male' && (
                   <IoMaleSharp className="absolute h-1/4 w-1/4 bottom-1 left-1 text-blue-300" />
                 )}
+                {/* lol did i even implement this? */}
                 {collected[_.uid] && (
                   <div className="absolute h-1/4 w-1/4 bottom-4 left-4 text-pink-600">GOT</div>
                 )}
